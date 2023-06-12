3aabb8c76c2cfd12ee8e51283b1d3b9f48ee536f
Greg Buzzard <buzzard@purdue.edu>
2023-06-12 10:54:54 -0400

    Improve the determination if the environment is active.

--- dev_scripts/clean_install_all.sh
+++ dev_scripts/clean_install_all.sh
@@ -3,18 +3,13 @@
 # Create and activate new conda environment
 # First check if the target environment is active and deactivate if so
 NAME=gpnp
-
-ENV_STRING=$((conda env list) | grep $NAME)
-if [[ $ENV_STRING == *$NAME* ]]; then
-  echo conda deactivate
+if [ "$CONDA_DEFAULT_ENV" == $NAME ]; then
+    conda deactivate
 fi
-cd ..
 
 # Then remove the old version and reinstall
-conda remove env --name gpnp --all
-conda create --name gpnp python=3.8
-conda activate gpnp
-pip install -r requirements.txt
-
-cd dev_scripts
+conda remove env --name $NAME --all
+conda create --name $NAME python=3.8
+conda activate $NAME
+pip install -r ../requirements.txt
 
